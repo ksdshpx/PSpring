3c5d1828bacad7c4c900b028921b04d6ec1fce73
ksdshpx <southeast_px@163.com>
2018-09-22 18:52:09 +0800

    创建Spring项目！

---
+++ src/cn/ksdshpx/spring/domain/Person.java
@@ -0,0 +1,23 @@
+package cn.ksdshpx.spring.domain;
+
+public class Person {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+
+	// 自定义方法
+	public void sayHell() {
+		System.out.println("Hello," + name);
+	}
+}
---
+++ src/cn/ksdshpx/spring/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package cn.ksdshpx.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.ksdshpx.spring.domain.Person;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// 1.创建Spring的IOC容器
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// 2.创建Person对象
+		Person person = (Person) applicationContext.getBean("person");
+		// 3.调用sayHello
+		person.sayHell();
+	}
+}
