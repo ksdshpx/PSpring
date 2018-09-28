d61adb971890c2b8e55f450a5c364be817b11e1a
ksdshpx <southeast_px@163.com>
2018-09-28 11:00:27 +0800

    bean与bean之间的继承关系以及抽象bean！

---
+++ src/cn/ksdshpx/spring/test/TestRelation.java
@@ -0,0 +1,22 @@
+package cn.ksdshpx.spring.test;
+/**
+ * @author peng.x
+ * @date 2018年9月28日 上午10:57:35
+ */
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.ksdshpx.spring.domain.Address;
+
+public class TestRelation {
+	@Test
+	public void testExtends() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Address address1 = ctx.getBean("address1", Address.class);
+		System.out.println(address1);
+		Address address2 = ctx.getBean("address2", Address.class);
+		System.out.println(address2);
+	}
+}
