9b1831bcb59f2c0d51614ae10f272052c0f1bb56
ksdshpx <southeast_px@163.com>
2018-10-08 15:01:15 +0800

    bean的作用域之prototype!

--- src/cn/ksdshpx/spring/test/TestScope.java
+++ src/cn/ksdshpx/spring/test/TestScope.java
@@ -18,4 +18,14 @@ public class TestScope {
 		Bike bike2 = ctx.getBean("bike", Bike.class);
 		System.out.println(bike == bike2);//true
 	}
+	
+	@Test
+	public void testPrototype() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Bike bike = ctx.getBean("bike2", Bike.class);
+		Bike bike2 = ctx.getBean("bike2", Bike.class);
+		System.out.println(bike == bike2);//false
+	}
+	
+	
 }
