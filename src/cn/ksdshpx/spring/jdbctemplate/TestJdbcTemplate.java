a7e6a664d9564b242538412a5745fb69eccb9ef6
ksdshpx <southeast_px@163.com>
2018-10-18 14:55:42 +0800

    JdbcTemplate的各种查询方法！

--- src/cn/ksdshpx/spring/jdbctemplate/TestJdbcTemplate.java
+++ src/cn/ksdshpx/spring/jdbctemplate/TestJdbcTemplate.java
@@ -7,7 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import cn.ksdshpx.spring.domain.User;
 
 /**
  * @author peng.x
@@ -37,5 +40,28 @@ public class TestJdbcTemplate {
 		paramsList.add(new Object[] { "wangWu", "tre45", 18, "male" });
 		paramsList.add(new Object[] { "zhaoLiu", "53teert", 88, "female" });
 		jdbcTemplate.batchUpdate(sql, paramsList);
+	}
+
+	@Test
+	public void testQueryForObjectReturnObj() {
+		String sql = "SELECT * FROM t_user WHERE id = ?";
+		Object[] params = { 3 };
+		User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), params);
+		System.out.println(user);
+	}
+
+	@Test
+	public void testQueryForObjectReturnVal() {
+		String sql = "SELECT COUNT(*) FROM t_user";
+		Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+		System.out.println(count);
+	}
+
+	@Test
+	public void testQuery() {
+		String sql = "SELECT * FROM t_user WHERE id > ?";
+		Object[] params = { 1 };
+		List<User> list = jdbcTemplate.query(sql, params, new BeanPropertyRowMapper<>(User.class));
+		System.out.println(list);
 	}
 }
